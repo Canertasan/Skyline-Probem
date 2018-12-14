c6edac7a01daa0953305127093ef10cd9804d961
Caner TAŞAN <canertasan@sabanciuniv.edu>
2018-12-14 06:12:12 +0300

    Update MPQ.h
    Some comments added to the code!

--- MPQ.h
+++ MPQ.h
@@ -6,18 +6,17 @@ using namespace std;
 
 enum LeftOrRight {LEFT,RIGHT};
 
-template <class Comparable>
+template <class Comparable> 
 struct ComparableAndLabel
-{
+{// Which will use in vector.
 	int label; //id
 	Comparable value; // coordinate
 	LeftOrRight LOR;
-	int height;
-	int index;
+	int height; 
 };
 
 template <class Comparable>
-class MPQ
+class MPQ // MAX HEAP ! 
 {
 public:
 	MPQ();
@@ -37,30 +36,30 @@ private:
 };
 
 template <class Comparable>
-MPQ<Comparable>::MPQ() // first push input 0
+MPQ<Comparable>::MPQ() // first push input 0 
 {
 	ComparableAndLabel<Comparable> cl;
 	cl.label = 0;
 	cl.value = 0;
 	arrayElement.push_back(cl);
-	currentSize = 0;
+	currentSize = 0; // Because first arrayElement is dumb.
 }
 
 template <class Comparable>
-vector<ComparableAndLabel<Comparable>> & MPQ<Comparable>::arrayElementGetter()
-{
+vector<ComparableAndLabel<Comparable>> & MPQ<Comparable>::arrayElementGetter() 
+{// to reach private value
 	return arrayElement;
 }
 
 template <class Comparable>
-MPQ<Comparable>::~MPQ()
-{
+MPQ<Comparable>::~MPQ() 
+{ // basic destructor.
 	arrayElement.clear();
 }
 
 template <class Comparable>
 int MPQ<Comparable>::currentSizeGetter()
-{
+{ // taking currentSize which is private
 	return currentSize;
 }
 
@@ -68,20 +67,20 @@ template <class Comparable>
 // In MPQC vector height will be the x cordinates and x will be height
 void MPQ<Comparable>::insert( const Comparable & x , int label ,LeftOrRight lor, int height)
 {
-	ComparableAndLabel<Comparable> cl;
+	ComparableAndLabel<Comparable> cl; // takin all element as for struct vector
 	cl.value = x;
 	cl.label = label;
 	cl.height = height;
 	cl.LOR = lor;
 	if(currentSize == arrayElement.size()-1)
-	{
-		arrayElement.resize(arrayElement.size()*2);
+	{// resize when it is in last element
+		arrayElement.resize(arrayElement.size()*2); 
 	}
 	// Percolate up
 	// Note that instead of swapping we move the hole up
 	int hole = ++currentSize;
 	for( ; hole > 1 && x > arrayElement[ hole / 2 ].value; hole /= 2 )
-	{
+	{ // its max heap then we need to carry up max one's.
 		arrayElement[ hole ] = arrayElement[ hole / 2 ];
 	}
 	arrayElement[ hole ]= cl;
@@ -99,31 +98,32 @@ void MPQ<Comparable>::Remove(const Comparable & x, int label ,LeftOrRight lor, i
 		if(arrayElement[i].label == label && arrayElement[i].value == x && arrayElement[i].LOR !=lor)
 			break;
 	}
-	arrayElement[i] = arrayElement[currentSize];
+	arrayElement[i] = arrayElement[currentSize]; // change place with deleted one and last element of vector
 	arrayElement[currentSize].value = 0;
 	arrayElement[currentSize].height = 0; 
 	arrayElement[currentSize].label = 0; 
 	arrayElement[currentSize].LOR = LEFT; 
-	currentSize--;
+	currentSize--; // then decrease 1 currentSize Bcs we delete element
 	ComparableAndLabel<Comparable> cl;
 	cl = arrayElement[i];
-	if(arrayElement[i].value > arrayElement[i/2].value ) // percolate up!!
-	{
+	//We need to keep as max heap ! 
+	if(arrayElement[i].value > arrayElement[i/2].value )  
+	{ // upside is bigger then percolate up!!
 		for( ; i > 1 && x > arrayElement[ i / 2 ].value; i /= 2 )
 		{
 			arrayElement[ i ] = arrayElement[ i / 2 ];
 		}
 		arrayElement[i] = cl;
 	}
-	else if(arrayElement[i].value < arrayElement[i/2].value) // percolate down!
-	{
+	else if(arrayElement[i].value < arrayElement[i/2].value) 
+	{//Otherwise then percolate down! 
 		percDown(arrayElement,i,currentSize);
 	}
 }
 
 template <class Comparable>
 bool MPQ<Comparable>::IsEmpty()
-{
+{// checking is it empty or not
 	return (currentSize == 0);
 }
 
@@ -131,7 +131,7 @@ template <class Comparable>  // for deleteMax
 // a is the array, i is the position to percolate down from
 // n is the logical size of the array
 void MPQ<Comparable>::percDown(vector<ComparableAndLabel<Comparable>> &a, int i, int n )
-{
+{ // It works with HeapSort! It is kind of organize as max heap after the swapping.
 	int child;
 	ComparableAndLabel<Comparable> tmp;
 	//tmp = arrayElement[ i ].value; // tmp is the item that will 
@@ -152,10 +152,10 @@ void MPQ<Comparable>::percDown(vector<ComparableAndLabel<Comparable>> &a, int i,
 
 template <class Comparable>
 void MPQ<Comparable>::heapSort(vector<ComparableAndLabel<Comparable>> &a)
-{
+{ // this is basic sort algorithm.
 	// sort
 	for(int j = currentSize; j >0; j--)
-	{
+	{ // first swap then prectDown. 
 		swap(a[1], a[j]);  // swap max to the last pos.
 		percDown(a,1,j-1); // re-form the heap
 	}
